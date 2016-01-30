a1cee004934ad795a40867fc399a8bbd388d1a8e
David Yang <dayy@linux.ucla.edu>
2016-01-29 17:13:43 -0800

    prep

--- create.sql
+++ create.sql
@@ -1,35 +1,35 @@
 CREATE TABLE Users
 (
-	UserID VARCHAR(20) PRIMARY KEY, 
-	Country VARCHAR(20), 
-	Address VARCHAR(50), 
+	UserID VARCHAR(20) PRIMARY KEY,
+	Country VARCHAR(20),
+	Address VARCHAR(50),
 	SellerRating INT
 );
 
 CREATE TABLE Items
 (
-	ItemID BIGINT PRIMARY KEY, 
-	UserID VARCHAR(20), 
-	ItemName VARCHAR(50), 
-	FirstBid DECIMAL(8,2), 
+	ItemID BIGINT PRIMARY KEY,
+	UserID VARCHAR(20),
+	ItemName VARCHAR(50),
+	FirstBid DECIMAL(8,2),
 	BuyPrice DECIMAL(8,2),
 	CurrentHighestBid DECIMAL(8,2),
 	StartTime TIMESTAMP,
 	EndTime TIMESTAMP,
-	NumberOfBids INT, 
+	NumberOfBids INT,
 	Description VARCHAR(4000)
 );
 
 CREATE TABLE ItemCategory
 (
-	ItemID BIGINT PRIMARY KEY, 
+	ItemID BIGINT,
 	Category VARCHAR(20)
 );
 
 CREATE TABLE Bids
 (
-	UserID VARCHAR(20) PRIMARY KEY, 
-	ItemID BIGINT, 
-	BidTime TIMESTAMP, 
+	UserID VARCHAR(20),
+	ItemID BIGINT,
+	BidTime TIMESTAMP,
 	Amount DECIMAL(8,2)
 );
